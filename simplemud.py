c7b992da218b22a41959aad93a682f4b5c08bce9
Karkaratz <pixelface@techie.com>
2024-01-16 23:01:09 +0100

    Added class player and refactored code for simplemud

--- simplemud.py
+++ simplemud.py
@@ -24,15 +24,16 @@ author: Mark Frimston - mfrimston@gmail.com
 import time
 
 import hashlib
+from player import Player
 game_name = "MUD LAND"
 
 
 def start_menu():
     return "********** Welcome Adventurer **********\n" + \
         "**********   Choose  Wisely   **********\n" + \
-        "1.signup\n" + \
-        "2.login\n" + \
-        "3.exit\n"
+        "signup\n" + \
+        "login\n" + \
+        "exit\n"
 
 def logged_in():
     return "************ Welcome Adventurer **********\n" + \
@@ -60,72 +61,67 @@ players = {}
 mud = MudServer()
 
 def signup(id, command):
-    if players[id]["phase2"]=="Pre-Login":
+    if players[id].get_phase2()=="Pre-Login":
         mud.send_message(id, "Enter email address: ")
         return "signup1"
-    elif players[id]["phase2"]=="signup1":
-        players[id]["email"]=command
+    elif players[id].get_phase2()=="signup1":
+        players[id].set_email(command)
         mud.send_message(id, "Enter password: ")
-        print (players[id]["email"])
+        print (players[id].get_email())
         return "signup2"
-    elif players[id]["phase2"]=="signup2":
+    elif players[id].get_phase2()=="signup2":
         enc=command.encode()
         passwd = hashlib.md5(enc).hexdigest()
-        players[id]["pass1"]=passwd
+        players[id].set_pass1(passwd)
         mud.send_message(id, "Confirm password: ")
-        print (players[id]["pass1"])
+        print (players[id].get_pass1())
         return "signup3"
-    elif players[id]["phase2"]=="signup3":
+    elif players[id].get_phase2()=="signup3":
         enc = command.encode()
         passwd = hashlib.md5(enc).hexdigest()
-        players[id]["pass2"]=passwd
-        if players[id]["pass2"] != players[id]["pass1"]:
+        players[id].set_pass2(passwd)
+        if players[id].get_pass2() != players[id].get_pass1():
             mud.send_message(id, "Passwords do not match.\nEnter Password:")
             return "signup2"
         mud.send_message(id, "Insert Character Name: ")
         return "signup4"
-    elif players[id]["phase2"] == "signup4":
-        players[id]["charname"] = command
+    elif players[id].get_phase2() == "signup4":
+        players[id].set_charname(command)
         with open("credentials.txt", "a") as f:
-                f.write(players[id]["email"] + ":")
-                f.write(players[id]["charname"] + ":")
-                f.write(players[id]["pass1"] + "\n")
+                f.write(players[id].get_email() + ":")
+                f.write(players[id].get_charname() + ":")
+                f.write(players[id].get_pass1() + "\n")
                 f.close()
                 mud.send_message(id,"You have registered successfully!")
-        players[id]["phase"] = None
-        players[id]["phase2"] = None
-        players[id]["pass1"] = None
-        players[id]["pass2"] = None
-        players[id]["email"] = None
-        players[id]["charname"] = None
+
         return "signupend"
 
 
 
 def login(id, command):
-    if players[id]["phase2"] == "registered1":
+    if players[id].get_phase2() == "registered1":
         mud.send_message(id, "Enter email address to Login: ")
         return "registered2"
-    elif players[id]["phase2"] == "registered2":
-        players[id]["email"] = command
+    elif players[id].get_phase2() == "registered2":
+        players[id].set_email(command)
         mud.send_message(id, "Enter password to Login: ")
         return "registered3"
-    elif players[id]["phase2"] == "registered3":
-        enc=command.encode()
-        players[id]["pass1"] = hashlib.md5(enc).hexdigest()
+    elif players[id].get_phase2() == "registered3":
+        enc = command.encode()
+        players[id].set_pass1(hashlib.md5(enc).hexdigest())
         user_ok = False
         with open("credentials.txt", "r") as f:
             lines = f.readlines()
             for line in lines:
                 stored_email, charname, stored_pwd = line.split(":")
-                if players[id]["email"] == stored_email:
+                if players[id].get_email() == stored_email:
                     user_ok = True
                     stored_pwd = stored_pwd.replace("\n", "")
                     break
             f.close()
-            if user_ok and players[id]["pass1"] == stored_pwd:
+            if user_ok and players[id].get_pass1() == stored_pwd:
                 mud.send_message(id, "{} logged in Successfully!".format(charname))
-                players[id]["charname"]=charname
+                players[id].set_charname(charname)
                 return "registeredend"
             else:
                 mud.send_message(id, "Login Failed!")
@@ -150,16 +146,7 @@ while True:
         # The dictionary key is the player's id number. We set their room to
         # None initially until they have entered a name
         # Try adding more player stats - level, gold, inventory, etc
-        players[id] = {
-            "email": None,
-            "charname" : None,
-            "room": None,
-            "phase": None,
-            "phase2": None,
-            "email" : None,
-            "pass1" : None,
-            "pass2" : None
-        }
+        players[id] = Player()
 
         # send the new player a prompt for their name
         mud.send_message(id, start_menu())
@@ -177,8 +164,7 @@ while True:
         for pid, pl in players.items():
             # send each player a message to tell them about the disconnected
             # player
-            mud.send_message(pid, "{} quit the game".format(
-                players[id]["charname"]))
+            mud.send_message(pid, "{} quit the game".format(players[id].get_charname()))
 
         # remove the player's entry in the player dictionary
         del (players[id])
@@ -193,45 +179,46 @@ while True:
 
         # if the player hasn't given their name yet, use this first command as
         # their name and move them to the starting room.
-        if players[id]["charname"] is None:
+        if players[id].get_charname() is None:
 
             if command == "exit":
                 mud._handle_disconnect(id)
-            elif command == "signup" or  players[id]["phase"] == "Pre-Login":
-                players[id]["phase"] = "Pre-Login"
-                if players[id]["phase2"] is None:
-                    players[id]["phase2"] = "Pre-Login"
-                players[id]["phase2"] = signup(id, command)
-                if players[id]["phase2"] == "signupend":
-                    players[id]["phase"] = "Registered"
-                    players[id]["phase2"] = "registered1"
+            elif command == "signup" or  players[id].get_phase() == "Pre-Login":
+                players[id].set_phase("Pre-Login")
+                if players[id].get_phase2() is None:
+                    players[id].set_phase2("Pre-Login")
+                players[id].set_phase2(signup(id, command))
+                if players[id].get_phase2() == "signupend":
+                    players[id] = Player()
+                    players[id].set_phase("Registered")
+                    players[id].set_phase2("registered1")
                     mud.send_message(id, start_menu())
-            elif command == "login" or  players[id]["phase"] == "Registered":
-                players[id]["phase"] = "Registered"
-                if players[id]["phase2"] is None:
-                    players[id]["phase2"] = "registered1"
-                players[id]["phase2"] = login(id, command)
-                if players[id]["phase2"] == "registeredend":
+            elif command == "login" or  players[id].get_phase() == "Registered":
+                players[id].set_phase("Registered")
+                if players[id].get_phase2() is None:
+                    players[id].set_phase2("registered1")
+                players[id].set_phase2(login(id, command))
+                if players[id].get_phase2() == "registeredend":
 
-                    players[id]["phase"] = "Game"
-                    players[id]["phase2"] = None
+                    players[id].set_phase("Game")
+                    players[id].set_phase2(None)
                     mud.send_message(id, logged_in())
-                    players[id]["phase"] = None
-                    players[id]["room"] = "Tavern"
+                    players[id].set_phase(None)
+                    players[id].set_room("Tavern")
 
                     # go through all the players in the game
                     for pid, pl in players.items():
                         # send each player a message to tell them about the new player
                         mud.send_message(pid, "{} entered the game".format(
-                            players[id]["charname"]))
+                            players[id].get_charname()))
 
                     # send the new player a welcome message
                     mud.send_message(id, "Welcome to the game, {}. ".format(
-                        players[id]["charname"])
+                        players[id].get_charname())
                                      + "Type 'help' for a list of commands. Have fun!")
 
                     # send the new player the description of their current room
-                    mud.send_message(id, rooms[players[id]["room"]]["description"])
+                    mud.send_message(id, rooms[players[id].get_room()]["description"])
 
         # each of the possible commands is handled below. Try adding new
         # commands to the game!
@@ -256,16 +243,16 @@ while True:
             # go through every player in the game
             for pid, pl in players.items():
                 # if they're in the same room as the player
-                if players[pid]["room"] == players[id]["room"]:
+                if players[pid].getroom() == players[id].get_room():
                     # send them a message telling them what the player said
                     mud.send_message(pid, "{} says: {}".format(
-                        players[id]["charname"], params))
+                        players[id].get_charname(), params))
 
         # 'look' command
         elif command == "look":
 
             # store the player's current room
-            rm = rooms[players[id]["room"]]
+            rm = rooms[players[id].get_room()]
 
             # send the player back the description of their current room
             mud.send_message(id, rm["description"])
@@ -274,11 +261,11 @@ while True:
             # go through every player in the game
             for pid, pl in players.items():
                 # if they're in the same room as the player
-                if players[pid]["room"] == players[id]["room"]:
+                if players[pid].get_room() == players[id].get_room():
                     # ... and they have a name to be shown
-                    if players[pid]["charname"] is not None:
+                    if players[pid].get_charname() is not None:
                         # add their name to the list
-                        playershere.append(players[pid]["charname"])
+                        playershere.append(players[pid].get_charname())
 
             # send player a message containing the list of players in the room
             mud.send_message(id, "Players here: {}".format(
@@ -297,7 +284,7 @@ while True:
             ex = params.lower()
 
             # store the player's current room
-            rm = rooms[players[id]["room"]]
+            rm = rooms[players[id].getroom()]
 
             # if the specified exit is found in the room's exits list
             if ex in rm["exits"]:
@@ -306,32 +293,32 @@ while True:
                 for pid, pl in players.items():
                     # if player is in the same room and isn't the player
                     # sending the command
-                    if players[pid]["room"] == players[id]["room"] \
+                    if players[pid].get_room() == players[id].get_room() \
                             and pid != id:
                         # send them a message telling them that the player
                         # left the room
                         mud.send_message(pid, "{} left via exit '{}'".format(
-                            players[id]["charname"], ex))
+                            players[id].get_charname(), ex))
 
                 # update the player's current room to the one the exit leads to
-                players[id]["room"] = rm["exits"][ex]
-                rm = rooms[players[id]["room"]]
+                players[id].set_room(rm["exits"][ex])
+                rm = rooms[players[id].get_room()]
 
                 # go through all the players in the game
                 for pid, pl in players.items():
                     # if player is in the same (new) room and isn't the player
                     # sending the command
-                    if players[pid]["room"] == players[id]["room"] \
+                    if players[pid].get_room() == players[id].get_room() \
                             and pid != id:
                         # send them a message telling them that the player
                         # entered the room
                         mud.send_message(pid,
                                          "{} arrived via exit '{}'".format(
-                                             players[id]["charname"], ex))
+                                             players[id].get_charname(), ex))
 
                 # send the player a message telling them where they are now
                 mud.send_message(id, "You arrive at '{}'".format(
-                    players[id]["room"]))
+                    players[id].get_room()))
 
             # the specified exit wasn't found in the current room
             else:
